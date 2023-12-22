64abd239e758816bc39db8ef0bf2e16b9f06c22d
Naren Janardhan Sulegai <narenjanardhan.sulegai@yahooinc.com>
2023-12-22 09:12:26 -0800

    Initial setup

---
+++ packages/app/.storybook/test-runner.js
@@ -0,0 +1,22 @@
+module.exports = {
+    // Hook that is executed before the test runner starts running tests
+    setup() {
+        // Add your configuration here.
+    },
+    /* Hook to execute before a story is initially visited before being rendered in the browser.
+     * The page argument is the Playwright's page object for the story.
+     * The context argument is a Storybook object containing the story's id, title, and name.
+     */
+    async preVisit(page, context) {
+        console.log('pre')
+        // Add your configuration here.
+    },
+    /* Hook to execute after a story is visited and fully rendered.
+     * The page argument is the Playwright's page object for the story
+     * The context argument is a Storybook object containing the story's id, title, and name.
+     */
+    async postVisit(page, context) {
+        console.log('post')
+        // Add your configuration here.
+    },
+};
